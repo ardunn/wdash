f3f9310e0b81c5bec352e5d36cf1357995035d9a
Alex Dunn <denhaus@gmail.com>
2021-06-25 15:40:47 -0700

    update run script

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-python daemon.py &
-python app.py &
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+LOGFILE="$DIR/log.txt"
+nohup python "$DIR/daemon.py" > $LOGFILE &
+nohup python "$DIR/app.py" > $LOGFILE &
+
