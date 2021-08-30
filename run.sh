da5c6c11e884e80c8cc6a43e1d37e6259eb5d923
Alex Dunn <denhaus@gmail.com>
2021-08-29 23:49:28 -0700

    update run.sh to include stderr

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 LOGFILE="$DIR/log.txt"
-nohup python "$DIR/daemon.py" > $LOGFILE &
-nohup python "$DIR/app.py" > $LOGFILE &
+nohup python "$DIR/daemon.py"  2>&1 $LOGFILE &
+nohup python "$DIR/app.py" 2>&1 $LOGFILE &
 
